7cd303e5eb741f097a6898cb0ab8f34496bc76a9
RichA <rich_ay@sbcglobal.net>
2026-04-15 21:37:45 -0500

    Added more functions singly linked lists.

--- Single_Linked_List/basics.py
+++ Single_Linked_List/basics.py
@@ -22,13 +22,81 @@ class SinglyLinkedList:
         self.tail = None
         self.size = 0
 
+##################################################################################
+    def iter( self ):
+        # This function is a 'generator' saving its state between calls.
+        current = self.head
+        
+        while current:
+            val = current.data
+            current = current.next  # move on to the next item in the list
+            yield val
+
+
+##################################################################################
     def append( self, data ):
         # Encapsulate the data in a Node.
         node = Node( data )
-        if self.tail:       # add at the end of the list
-            self.tail.next = node
-            self.tail = node
+        if self.tail:             # add at the end of the list
+            self.tail.next = node # update the current node to point to the new node
+            self.tail = node      # set the tail to the new node
 
-        else:               # the first entry in an empty list
+        else:                     # the first entry in an empty list
             self.head = node
             self.tail = node
+
+        self.size += 1
+
+##################################################################################
+    def append_at_a_location( self, data, index ):
+        # Start at the 'head' and traverse to 'index', then insert/append.
+        current  = self.head
+        previous = self.head
+
+        # Encapsulate the new data into a node.
+        node = Node( data )
+
+        count = 1
+        while current:
+            if index == 1:       # append at the start of the list
+                node.next = current
+                self.head = node
+                print( "Appending to the list start." )
+                self.size += 1
+                return
+
+            elif index == count: # insertion point found
+                node.next     = current
+                previous.next = node
+                self.size     += 1
+                return
+            
+            else:                # move forward to the next node
+                count += 1
+                previous = current
+                current  = current.next
+
+        if count < index:
+            print( "The list has too few nodes for this insertion." )
+
+##################################################################################
+    def list_size( self) :
+        # Simply traverse the list and count the items.
+        count = 0
+        current = self.head
+
+        while current:
+            count +=1
+            current = current.next
+
+        return count
+
+##################################################################################
+    def search( self, data ):
+        # Search the list for a specific data item.
+        for node in self.iter( ):
+            if data == node:
+                return True
+            
+        return False
+    
--- Single_Linked_List/single_list.py
+++ Single_Linked_List/single_list.py
@@ -6,6 +6,19 @@ import basics
 ####################################################################################################
 ####################################################################################################
 
+# Function to print the current state of the list
+
+def print_list( ):
+    # Traverse the list and output the data.
+    current = food.head
+    while current:
+        print( 'The nodes data is: ', current.data )
+        current = current.next      # move to the next node if it exists.
+
+    print( ' ' )
+
+
+####################################################################################################
 # Create sample nodes for the linked list.  Begin by defining the primary data.
 
 food = basics.SinglyLinkedList()
@@ -13,10 +26,33 @@ food = basics.SinglyLinkedList()
 food.append( 'eggs' )
 food.append( 'ham' )
 food.append( 'spam' )
+print( ' ' )
+print( 'The initial list.' )
+print( 'The list size is: ', food.size )
+print_list()
 
-# Traverse the list and output the data.
-current = food.head
-while current:
-    print( 'The nodes data is: ', current.data )
-    current = current.next      # move to the next node if it exists.
 
+
+
+# Test the 'append_at_a_location' function.
+food.append_at_a_location( 'bacon', 2 )
+print( 'The list after appending "bacon" as the 2nd item.' )
+print( 'The list size is: ', food.size )
+print_list()
+
+
+food.append_at_a_location( 'cheese', 1 )
+print( 'The list after appending "cheese" to the beginning.' )
+print( 'The list size is: ', food.size )
+print_list()
+
+food.append_at_a_location( 'tomatoes', 10 )   # past the end of the list
+print( 'The list size is: ', food.size )
+print_list()
+
+print( 'The current list size is: ', food.list_size() )
+
+# Test the 'search' function.
+print( ' ' )
+print( 'Is "cheese" in the list?', food.search( 'cheese' ) )
+print( 'Is "tomatoes" in the list?', food.search( 'tomatoes' ) )
