88e59a85a606f42f3b2fba595e224a19c669703d
RichA <rich_ay@sbcglobal.net>
2026-04-22 21:45:08 -0500

    Added code to illustrate stack operations using a linked list.

---
+++ Stack/st-class.py
@@ -0,0 +1,54 @@
+""" Class definitions for stack operations using linked lists."""
+
+
+"""The 'Node' class defines each item/entry in the (single linked) list."""
+class Node:
+    """ Initializer / Constructor"""
+    def __init__ ( self, data=None ):
+        self.data = data
+        self.next = None         # the end of the list is 'none'
+
+class Stack:
+    """ Initializer / Constructor"""
+    def __init__( self ):
+        self.top  = None
+        self.size = 0            # the stack size is zero
+
+
+#################################################################################
+    def push( self, data ):
+        # Define the operation of pushing a value onto the stack.
+
+        # Create the new node for the stack (list).
+        node = Node( data )
+
+        if self.top:
+            # The stack is not empty, add the node and update the pointers
+            node.next = self.top    # point to previous top of stack
+            self.top  = node        # the new top is the new node
+
+        else:
+            # For an empty stack, point to the new node
+            self.top = node
+        
+        self.size += 1              # increment the size of the stack
+
+
+#################################################################################
+    def pop( self ):
+        # Define the operation of popping a value off of the stack.
+
+        if self.top :               # the stack is not empty
+            data = self.top.data
+            self.size -= 1
+
+            if self.top.next:       # there are more nodes
+                self.top = self.top.next   # point to the next node in the stack
+
+            else:
+                self.top = None
+            
+            return data
+        
+        else:
+            print( '\nThe stack is empty, nothing to pop.' )
